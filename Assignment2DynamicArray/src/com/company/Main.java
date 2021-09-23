4df4ed65b90b4113bc746b990152a78d1de2824b
Vitaliy Vuychych <vitaliy202@gmail.com>
2021-09-23 12:23:50 -0400

    Looks completed to me

--- Assignment2DynamicArray/src/com/company/Main.java
+++ Assignment2DynamicArray/src/com/company/Main.java
@@ -12,9 +12,14 @@ public class Main {
         cart.add("apple");
         cart.add("banana");
         cart.add("pear", 3);
+        cart.add("grapefruit", 2);
+        cart.add("lettuce");
+        cart.add("oranges");
 
         cart.replace("potatoes", 1);
         cart.replace("test_item", -1);  // To test if anything happens when using an incorrect index
+
+        cart.remove(2);
 
         System.out.println(cart.getIndex("carrot"));
         System.out.println(cart.getIndex("potatoes"));
@@ -23,3 +28,7 @@ public class Main {
         cart.printArray();
     }
 }
+
+// I probably could have added an interaction with a user
+// where they would choose what to add, remove, view or otherwise modify
+// through the console, but was not sure if that was necessary.
