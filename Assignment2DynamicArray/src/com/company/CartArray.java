faf0ebaa6adf70feed00d9f8f7a1f95ef0b7e2fd
Vitaliy Vuychych <vitaliy202@gmail.com>
2021-09-22 21:44:28 -0400

    commit

--- Assignment2DynamicArray/src/com/company/CartArray.java
+++ Assignment2DynamicArray/src/com/company/CartArray.java
@@ -12,11 +12,22 @@ public class CartArray {
     // Looping through the array and printing it out
     public void printArray(){
         for(int i = 0; i < length; i++){
-            System.out.println(items[i]);
+            System.out.println("#" + (i + 1) + ": " + items[i]);
         }
     }
 
     public void insert(String item){
+        if(items.length == length){
+            String[] itemsNew = new String[length + 1];     // Creates new array
 
+            for(int i = 0; i < length; i++){                // Copy the old array
+                itemsNew[i] = items[i];
+            }
+
+            items = itemsNew;                               // Make the new array the main one
+        }
+
+        items[length] = item;                               // Adding the item
+        length++;
     }
 }
--- Assignment2DynamicArray/src/com/company/Main.java
+++ Assignment2DynamicArray/src/com/company/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         // Declaring array object
-        CartArray cart = new CartArray(10);
+        CartArray cart = new CartArray(2);
+        cart.insert("carrot");
+        cart.insert("grapes");
+        cart.insert("apple");
+        cart.insert("banana");
         cart.printArray();
     }
 }
