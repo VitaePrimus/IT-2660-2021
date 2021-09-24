5984dd466aed23ac20c63d09d94fa275d5e0494b
Vitaliy Vuychych <vitaliy202@gmail.com>
2021-09-24 10:25:17 -0400

    Looks completed, just to double check

--- Assignment3Stacks/src/com/company/Main.java
+++ Assignment3Stacks/src/com/company/Main.java
@@ -3,8 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Assignment 3 Start");
-
         Stack stack = new Stack(1);
 
         stack.push("one");
@@ -17,6 +15,43 @@ public class Main {
         stack.peek();
         stack.pop();
         stack.peek();
+        stack.pop();
+        stack.peek();
+        stack.push("number");
+        stack.peek();
+        stack.pop();
+        stack.peek();
+        stack.pop();
+        stack.peek();
+
+        stack.pop();    // Becomes empty
+        stack.peek();
+        stack.pop();    // Can't remove anything because IS_EMPTY
+        stack.peek();
+
+        stack.push("another string");
+        stack.peek();
+        stack.push("and another string");
+        stack.peek();
+        stack.push("and another another string");
+        stack.peek();
 
     }
 }
+
+/*Output should read this:
+one
+two
+three
+four
+three
+two
+number
+two
+one
+Stack is empty.
+Nothing to remove. Stack is empty.
+Stack is empty.
+another string
+and another string
+and another another string*/
--- Assignment3Stacks/src/com/company/Stack.java
+++ Assignment3Stacks/src/com/company/Stack.java
@@ -2,50 +2,79 @@ package com.company;
 
 public class Stack {
     private String[] string;
-    private int size;
+    private int size;           // To keep track of an array size
 
     public Stack(int size){
-        string = new String[size];
+        string = new String[size];      // Initializing stack with preferred length;
     }
 
+    // Push to add to the "top"
     public void push(String string){
-        String[] newString = new String[size + 1];
 
-        if(this.string.length == size){
+        if(IS_FULL()){
+            String[] newString = new String[size + 1];
+
             for(int i = 0; i < size; i++){
-                newString[i] = this.string[i];
+                newString[i] = this.string[i];      // Copy data to a new array when old is full
             }
 
             this.string = newString;
         }
 
-        this.string[size] = string;
+        this.string[size] = string;     // Add item
         size++;
     }
 
+    // Pop to remove from the "top"
     public void pop(){
-        String[] newString = new String[size - 1];
-
-        if(this.string.length == size){
-            for(int i = 0; i < (size - 1); i++){
-                newString[i] = this.string[i];
-            }
-
-            this.string = newString;
+        if(IS_EMPTY()){
+            System.out.println("Nothing to remove. Stack is empty.");
+            return;     // Do nothin, just return the message if empty
         }
 
+        String[] newString = new String[size - 1];      // New smaller array
+
+        for(int i = 0; i < (size - 1); i++){
+            newString[i] = this.string[i];
+        }
+
+        this.string = newString;
         size--;
-
     }
 
+    // Looking at the top element
     public void peek(){
-        System.out.println(this.string[size - 1]);
-
-    }
-
-    public void print(){
-        for(int i = 0; i < size; i++){
-            System.out.println("#" + (i + 1) + ": " + this.string[i]);
+        if(IS_EMPTY()){
+            System.out.println("Stack is empty.");
+            return;     // Return only the message if empty
         }
+        System.out.println(this.string[size - 1]);
     }
+
+    // Nice to have
+    public Boolean IS_FULL(){
+        boolean IS_FULL;
+
+        IS_FULL = this.string.length == size;
+
+        return IS_FULL;
+    }
+
+    // This helps
+    public Boolean IS_EMPTY(){
+        boolean IS_EMPTY;
+
+        IS_EMPTY = this.string.length <=0;
+
+        return IS_EMPTY;
+    }
+
+
+
+//    Not needed for stacks, left for testing
+//    public void print(){
+//        for(int i = 0; i < size; i++){
+//            System.out.println("#" + (i + 1) + ": " + this.string[i]);
+//        }
+//    }
 }
